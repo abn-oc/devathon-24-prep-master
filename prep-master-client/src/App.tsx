b7410b65e5006d9137ccd97902f4d7b218476f73
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-09-07 14:56:08 +0500

    Revert "Merge branch 'main' of https://github.com/thomas-catt/devathon-24"
    This reverts commit 3090617a400a066c8f19cf1828ee3e0552850a8f, reversing
    changes made to 42b997f0900bb4f6ae9d9e16f001e103d2b07b62.

--- prep-master-client/src/App.tsx
+++ prep-master-client/src/App.tsx
@@ -6,25 +6,17 @@ import Login from './Pages/Login';
 import NavBar from './Components/NavBar';
 import TeacherDashboard from './Pages/TeacherDashboard';
 import CreateTest from './Pages/CreateTest';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const token = localStorage.getItem('prep-token');
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/login' element={<Login />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path='/home' element={<Home />} />
-          <Route path='/teacherdashboard' element={<TeacherDashboard />} />
-          <Route path='/teacherdashboard/createtest' element={<CreateTest />} />
-        </Route>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/teacherdashboard' element={<TeacherDashboard/>}/>
+        <Route path='/teacherdashboard/createtest' element={<CreateTest/>}/>
       </Routes>
     </div>
   );
