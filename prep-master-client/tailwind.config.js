9e122dfa2465bd4142d8343e2138b4ff08238aef
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-09-07 10:56:57 +0500

    primary color

--- prep-master-client/tailwind.config.js
+++ prep-master-client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6d28d9",
+          50: "#f5f3ff",
+          100: "#e6d9ff",
+          150: "#d6baff",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
