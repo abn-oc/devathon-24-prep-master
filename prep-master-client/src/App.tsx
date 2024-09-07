6e44910d2eb73ce36911bd1e2bd6ebbb891ac811
abn-oc <abdullah.iht22@gmail.com>
2024-09-07 10:13:12 +0500

    made starting pages

--- prep-master-client/src/App.tsx
+++ prep-master-client/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'
+import Landing from './Pages/Landing';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<h1>starting page</h1>}/>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   );
